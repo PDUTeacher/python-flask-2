c23284abe052781b18c66897bdfe6197e603e571
PDUTeacher <tmv.developer@gmail.com>
2024-06-29 14:14:01 +0300

    add templates

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    n = random.randint(0, 1000)
-    k = random.randint(1000, 5000)
-    return render_template('index.html', n=n, k=k)
+@app.route('/name')
+def index(name=''):
+    return render_template('index.html', name=name)
 
 
 @app.route('/login')
@@ -16,5 +14,21 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+@app.route('/details')
+def details():
+    return render_template('details.html')
+
+
+# Лише для локаотного сервера (закоментувати)
 if __name__ == '__main__':
     app.run(debug=True)
